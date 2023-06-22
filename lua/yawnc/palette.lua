823047a3742d41e2eae0d5c49e47b1206f8a12b4
sonjiku <57968409+sonjiku@users.noreply.github.com>
2023-06-22 18:19:43 +0300

    These defaults will probably be preffered until I actually finish iconema and add it as a backend to pywal.

--- lua/yawnc/palette.lua
+++ lua/yawnc/palette.lua
@@ -2,22 +2,40 @@ vim.cmd [[ source $HOME/.cache/wal/colors-wal.vim ]]
 -- local background = vim.g.background
 -- local foreground = vim.g.foreground
 -- local cursor = vim.g.cursor
+-- local wal00 = vim.g.color0
+-- local wal01 = vim.g.color1
+-- local wal02 = vim.g.color2
+-- local wal03 = vim.g.color3
+-- local wal04 = vim.g.color4
+-- local wal05 = vim.g.color5
+-- local wal06 = vim.g.color6
+-- local wal07 = vim.g.color7
+-- local wal08 = vim.g.color8
+-- local wal09 = vim.g.color9
+-- local wal10 = vim.g.color10
+-- local wal11 = vim.g.color11
+-- local wal12 = vim.g.color12
+-- local wal13 = vim.g.color13
+-- local wal14 = vim.g.color14
+-- local wal15 = vim.g.color15
+
 local wal00 = vim.g.color0
-local wal01 = vim.g.color1
-local wal02 = vim.g.color2
-local wal03 = vim.g.color3
-local wal04 = vim.g.color4
-local wal05 = vim.g.color5
-local wal06 = vim.g.color6
-local wal07 = vim.g.color7
-local wal08 = vim.g.color8
-local wal09 = vim.g.color9
+local wal01 = vim.g.color8
+local wal02 = vim.g.color7
+local wal03 = vim.g.color15
+local wal04 = vim.g.color7
+local wal05 = vim.g.color7
+local wal06 = vim.g.color15
+local wal07 = vim.g.color15
+local wal08 = vim.g.color9
+local wal09 = vim.g.color3
 local wal10 = vim.g.color10
 local wal11 = vim.g.color11
 local wal12 = vim.g.color12
 local wal13 = vim.g.color13
 local wal14 = vim.g.color14
-local wal15 = vim.g.color15
+local wal15 = vim.g.color2
+
 
 
 local palette = {
